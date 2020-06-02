c7708ab30ac39ad2196c3b88edd86463cf6e4ea9
李成 <lf97895503@gmail.com>
2020-06-02 23:55:57 +0800

    添加为递归方式

--- mian_shi_64.js
+++ mian_shi_64.js
@@ -8,3 +8,15 @@ var sumNums = function (n) {
   }
   return recursive(n);
 }
+
+// todo 改成了 尾递归  具体递归是否生效还得再看  时间击败百分之九 空间击败 100%
+var sumNums = function (n) {
+  function recursive (num, acc) {
+    if (num == 1) {
+      return acc+1;
+    } else {
+      return recursive(num-1, acc+num);
+    }
+  }
+  return recursive(n, 0);
+}
